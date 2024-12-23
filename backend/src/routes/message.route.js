704554199c7d9406be77ffed4d9ea646a05f4133
Vaibhav Dingalwar <132747124+vaibhavdingalwar26@users.noreply.github.com>
2024-12-23 20:46:50 +0530

    Complete Project

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
+
+const router = express.Router();
+
+// Correct middleware name: protectRoute
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
+
+export default router;
